f90a1f422802400ea371a3e2a649aa3f7767f8df
unknown <farhanalktr91@gmail.com>
2024-10-06 19:03:57 +0700

    header sidebar dashboard

--- src/store/store.mjs
+++ src/store/store.mjs
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import adminProductsSlice from "./admin/productSlice";
 import authReducer from "./authSlice";
+import commonFeatureSlice from "./commonSlice";
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    adminProducts: adminProductsSlice,
+    commonFeature: commonFeatureSlice,
   },
 });
 
