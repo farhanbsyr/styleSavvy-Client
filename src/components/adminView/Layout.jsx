a00cb317b470b8311c8d33b6d738cc955d741392
unknown <farhanalktr91@gmail.com>
2025-06-19 11:37:45 +0700

    fix: path

--- src/components/adminView/Layout.jsx
+++ src/components/adminView/Layout.jsx
@@ -1,19 +1,19 @@
 import { Outlet } from "react-router-dom";
-import AdminSideBar from "./sidebar";
-import AdminHeader from "./header";
+import AdminSideBar from "./Sidebar";
+import AdminHeader from "./Header";
 import { useState } from "react";
 
 function AdminLayout() {
   const [openSidebar, setOpenSidebar] = useState(false);
 
   return (
-    <div className="flex min-h-screen w-full">
+    <div className="flex w-full min-h-screen">
       {/* admin sidebar */}
       <AdminSideBar open={openSidebar} setOpen={setOpenSidebar} />
-      <div className="flex flex-1 flex-col">
+      <div className="flex flex-col flex-1">
         {/* admin header */}
         <AdminHeader setOpen={setOpenSidebar} />
-        <main className="flex-1 flex-col flex bg-muted/40 p-4 md:p-6">
+        <main className="flex flex-col flex-1 p-4 bg-muted/40 md:p-6">
           <Outlet />
         </main>
       </div>
