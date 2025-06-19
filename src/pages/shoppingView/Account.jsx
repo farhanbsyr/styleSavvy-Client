3be52190fffec6fe8b411bcef27167f5ceb6fd7e
unknown <farhanalktr91@gmail.com>
2025-06-19 11:45:10 +0700

    fix: path import

--- src/pages/shoppingView/Account.jsx
+++ src/pages/shoppingView/Account.jsx
@@ -1,6 +1,6 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import accImg from "../../assets/account.jpg";
-import Address from "@/components/shoppingView/address";
+import Address from "@/components/shoppingView/Address";
 import ShoppingOrders from "@/components/shoppingView/Ordes";
 // import ShoppingOrders from "@/components/shopping-view/orders";
 
@@ -10,11 +10,11 @@ function ShoppingAccount() {
       <div className="relative h-[300px] w-full overflow-hidden">
         <img
           src={accImg}
-          className="h-full w-full object-cover object-center"
+          className="object-cover object-center w-full h-full"
         />
       </div>
-      <div className="container mx-auto grid grid-cols-1 gap-8 py-8">
-        <div className="flex flex-col rounded-lg border bg-background p-6 shadow-sm">
+      <div className="container grid grid-cols-1 gap-8 py-8 mx-auto">
+        <div className="flex flex-col p-6 border rounded-lg shadow-sm bg-background">
           <Tabs defaultValue="orders">
             <TabsList>
               <TabsTrigger value="orders">Orders</TabsTrigger>
